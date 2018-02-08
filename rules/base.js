2254aeb17ecb9b52da0749ed0384b55d5624c798
Sasha <alex@backendless.consulting>
2018-02-08 12:13:22 +0200

    wc

--- rules/base.js
+++ rules/base.js
@@ -97,7 +97,6 @@ module.exports = {
         "no-new-wrappers": 2,
         "no-octal": 2,
         "no-octal-escape": 2,
-        "no-param-reassign": [2, { "props": true }],
         "no-proto": 2,
         "no-redeclare": 2,
         "no-script-url": 2,
